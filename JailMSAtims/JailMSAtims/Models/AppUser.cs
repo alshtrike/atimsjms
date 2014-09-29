7496583cbc3a0e8fdf17deb173d6cdf0ec6b2151
alshtrike <alshtrike@gmail.com>
2014-09-29 11:04:44 -0700

    Official JMS Project

---
+++ JailMSAtims/JailMSAtims/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JailMSAtims.Models
+{
+    public class AppUser
+    {
+        public int ID {get; set;}
+        public string usename { get; set; }
+        public string password { get; set; }
+
+    }
+}

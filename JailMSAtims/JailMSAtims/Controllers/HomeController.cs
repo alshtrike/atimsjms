d7c9efa04a4f16637bb1e0f548b9ff420c22030f
alshtrike <alshtrike@gmail.com>
2014-10-06 12:41:46 -0700

    Required Authorization

--- JailMSAtims/JailMSAtims/Controllers/HomeController.cs
+++ JailMSAtims/JailMSAtims/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace JailMSAtims.Controllers
-{
+{    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()

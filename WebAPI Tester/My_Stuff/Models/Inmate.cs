50f3c7f19945922af1733b33a95c2d765ea90ded
garrett.robertson <the_aboutji@hotmail.com>
2015-02-02 18:47:26 -0800

    Copying from Collin Branch 2

---
+++ WebAPI Tester/My_Stuff/Models/Inmate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_Tester.Models
+{
+    public class Inmate
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}

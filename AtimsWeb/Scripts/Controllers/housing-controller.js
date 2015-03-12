e17b1381dd5cc82739ff44a8a7dfca9dda5c3bee
alshtrike <alshtrike@gmail.com>
2015-03-12 13:27:13 -0700

    tabs in tabs! ... in tabs...in tabs?

---
+++ AtimsWeb/Scripts/Controllers/housing-controller.js
@@ -0,0 +1,14 @@
+﻿atimsApp.controller('HousingController', function ($scope) {
+    $scope.tabs = [
+        {
+            title: 'Closet',
+            subTabs: []
+        },
+        {
+            title: 'Supply',
+            subTabs: [{ title: 'Item' }, { title: 'Check List' }, { title: 'History' }, {title:'Manage'}]
+        }
+
+
+    ]
+});

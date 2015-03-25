df4369e9b2a01516124b490a580ed98ac9963eb0
alshtrike <alshtrike@gmail.com>
2015-03-25 11:38:43 -0700

    push 2

---
+++ AtimsWeb/JS/Controllers/intake-controller.js
@@ -0,0 +1,25 @@
+﻿atimsApp.controller('IntakeController', function ($scope, $http) {
+
+    
+
+    $scope.addInmate = function () {
+
+        $http.post('/api/Inmates/', this.newInmate).success(function (data) {
+            alert("Added Successfully!!");
+        }).error(function (data) {
+            $scope.error = "An Error has occured while Adding Inmate: " + data;
+        });
+    };
+
+
+    $http.get('/api/Facilities/').success(function (data) {
+        $scope.facilities = data;
+        
+    })
+    .error(function () {
+        $scope.error = "An Error has occured while loading posts!";
+    });
+    $scope.changeFac = function (fac) {
+        $scope.Facility = fac.FacilityName;
+    };
+});

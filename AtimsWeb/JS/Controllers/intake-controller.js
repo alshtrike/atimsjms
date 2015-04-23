a6c73b22b34f29dde631dd331ad16b2f29b7c69b
alshtrike <alshtrike@gmail.com>
2015-04-23 15:13:02 -0700

    Fancy Validation Intake, Progress Demo PreBook

--- AtimsWeb/JS/Controllers/intake-controller.js
+++ AtimsWeb/JS/Controllers/intake-controller.js
@@ -1,6 +1,7 @@
 ﻿atimsApp.controller('IntakeController', function ($scope, $http) {
 
-    
+    $scope.prebookSection = "Views/JMS/Intake/PreBook1.cshtml";
+    var currentPage = 1;
 
     $scope.addInmate = function () {
 
@@ -22,4 +23,27 @@
     $scope.changeFac = function (fac) {
         $scope.Facility = fac.FacilityName;
     };
-});
+
+    $scope.updateProgress = function (direction) {
+        //85 is the width of each step
+        
+        if(direction === 'next'){
+            //increase width
+            currentPage++;
+            if(($('.completion').width()/85)+1 == $('li').first().text()){ 
+            }
+            $('.completion').animate({'width':'+=85px'},1500);
+        }else{
+            //decrease width
+            currentPage--;
+            $('.completion').animate({'width':'-=85px'},1500);
+        }
+        $scope.prebookSection = "Views/JMS/Intake/PreBook"+currentPage+".cshtml";
+    }
+    $scope.jumpToSection = function (section) {
+        width = 85 * section;
+        $('.completion').animate({ 'width': width + 'px' }, 1500);
+        $scope.prebookSection = "Views/JMS/Intake/PreBook" + section + ".cshtml";
+    }
+  }
+);
